0800e1cc5cd97b04d3f6831ed772be6cd15b232c
AlokTiwari311 <AlokTiwari311113@gmail.com>
2024-11-26 13:19:20 +0530

    Added homepage view and fixed import issue

--- campaign_manager/views.py
+++ campaign_manager/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
 
 def homepage(request):
-    return HttpResponse("Welcome to the Campaign Manager API!")
+    return HttpResponse("""
+        <h1>Welcome to the Campaign Manager API!</h1>
+        <p>Available endpoints:</p>
+        <ul>
+            <li><a href="/api/agents/">/api/agents/</a></li>
+            <li><a href="/api/campaigns/">/api/campaigns/</a></li>
+            <li><a href="/api/campaign-results/">/api/campaign-results/</a></li>
+        </ul>
+    """)
