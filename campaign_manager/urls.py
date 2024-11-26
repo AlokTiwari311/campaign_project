05a02d3f82cb1499e3e9381150158c782424bab5
AlokTiwari311 <AlokTiwari311113@gmail.com>
2024-11-26 13:13:33 +0530

    Added homepage view and fixed import issue

--- campaign_manager/urls.py
+++ campaign_manager/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import homepage  # Import the homepage view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),  # Your API routes
+    path("", homepage),  # Root URL
 ]
